00c67fc2635cc37c57c5399b3e3a2a2121f2ed96
rolanbar <rolanbar@fel.cvut.cz>
2017-05-02 16:54:14 +0200

    update

--- helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
+++ helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
@@ -26,7 +26,7 @@ public class HelloService {
 
     String createHelloMessage(String name) {
 	final String hostname = System.getenv("HOSTNAME");
-        return "Hi " + name + " from " + hostname + "!";
+        return "MY " + name + " from " + hostname + "!";
     }
 
 }
